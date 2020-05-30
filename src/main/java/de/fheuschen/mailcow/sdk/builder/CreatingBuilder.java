12a62550094a42be0a111d9c566b01a4257847cb
Julian Benedikt Heuschen <julian@fheuschen.de>
2020-05-30 19:33:13 +0200

    hard Builder requirement mechanism

--- src/main/java/de/fheuschen/mailcow/sdk/builder/CreatingBuilder.java
+++ src/main/java/de/fheuschen/mailcow/sdk/builder/CreatingBuilder.java
@@ -1,5 +1,6 @@
 package de.fheuschen.mailcow.sdk.builder;
 
+import de.fheuschen.mailcow.sdk.exception.BuilderException;
 import de.fheuschen.mailcow.sdk.util.Util;
 
 /**
@@ -14,8 +15,28 @@ public interface CreatingBuilder<P> extends RawBuilder<P> {
      */
     P create();
 
+    /**
+     * Returns the values of all required fields. Used to check for requirement fulfillment.
+     * @return
+     */
     Object[] getRequiredFields();
+
+    /**
+     * Returns whether all required fields have non-null values.
+     * @return
+     */
     default boolean requirementsFulfilled() {
         return !Util.isAnyNull(getRequiredFields());
+    }
+
+    /**
+     * Same as requirementsFulfilled() but throws a runtime exception if the requirements are not fulfilled.
+     * @see #requirementsFulfilled()
+     * @return true if fulfilled
+     */
+    default boolean hardRequirementsFulfilled() {
+        if(!requirementsFulfilled())
+            throw new BuilderException("Not all required fields have non-null values! To check which fields are required, please refer to the documentation.");
+        return true;
     }
 }
---
+++ src/main/java/de/fheuschen/mailcow/sdk/exception/BuilderException.java
@@ -0,0 +1,12 @@
+package de.fheuschen.mailcow.sdk.exception;
+
+/**
+ * BuilderException
+ *
+ * @author Julian B. Heuschen <julian@fheuschen.de>
+ */
+public class BuilderException extends MailcowRuntimeException {
+    public BuilderException(String message) {
+        super(message);
+    }
+}
